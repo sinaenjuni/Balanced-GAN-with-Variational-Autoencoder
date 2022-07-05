cb13d64d3dbbbce83ed9983f610a791793c01de3
sinaenjuni <y2k4388@gmail.com>
2022-07-05 16:27:07 +0900

    modify: fid score method, per class

--- src/metrics/gen_images.py
+++ src/metrics/gen_images.py
@@ -1,7 +1,8 @@
 import os
 import torch
 from models.gan.embedded_generator import Generator
-from torchvision.utils import save_image
+# from torchvision.utils import save_image
+from PIL import Image
 
 
 image_size = 64
@@ -23,7 +24,7 @@ g = Generator(image_size=image_size,
               num_class=num_classes,
               norm='bn').to(device)
 
-state = torch.load("/home/dblab/sin/git/VAE-GAN/src/save_files/evae/ebgan_cifar10/weights/g_10.pth")
+state = torch.load("/home/dblab/sin/git/VAE-GAN/src/save_files/evae/ebgan_cifar10/weights/g_99.pth")
 g.load_state_dict(state)
 
 for cls_ in range(num_classes):
@@ -40,7 +41,9 @@ for cls_ in range(num_classes):
         path_ = os.path.join(save_path, str(cls_))
         if not os.path.exists(path_):
             os.makedirs(path_)
-        gen_img = (gen_img * 0.5 + 0.5)
-        print(gen_img.min(), gen_img.max())
-        save_image(gen_img, path_ + f"/{idx + (1000 * cls_)}.png")
+        gen_img = ((gen_img * 0.5 + 0.5)*255.0).to(torch.uint8).permute(1,2,0).cpu().numpy()
+        gen_img = Image.fromarray(gen_img)
+        # print(gen_img.min(), gen_img.max())
+        # save_image(gen_img, path_ + f"/{idx + (1000 * cls_)}.png")
+        gen_img.save(path_ + f"/{idx + (1000 * cls_)}.png")
 
