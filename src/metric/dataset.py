804ba4fd0b0e666fac57be80868075a4100162de
sinaenjuni <y2k4388@gmail.com>
2022-07-18 20:40:57 +0900

    training: autoencoder and generative adversarial networks in EBGAN

--- src/metric/dataset.py
+++ src/metric/dataset.py
@@ -1,7 +1,10 @@
 import pytorch_lightning as pl
+import numpy as np
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import Compose, Normalize, ToTensor
+from torch.utils.data import Dataset
+
 
 class DataModule_(pl.LightningDataModule):
     def __init__(self, path_train, batch_size, num_workers=4, pin_memory=True):
@@ -33,6 +36,43 @@ class DataModule_(pl.LightningDataModule):
     #
     # def test_dataloader(self):
     #     return DataLoader(self.valid_dataset, batch_size=self.batch_size)
+
+
+
+class ContrastiveDataset(Dataset):
+    def __init__(self, dataset):
+        super(ContrastiveDataset, self).__init__()
+
+        data = dataset
+        self.images = data.data
+        self.labels = np.array(data.targets)
+
+        self.transforms = Compose([ToTensor(),
+                                   Normalize(mean=(0.5, 0.5, 0.5),
+                                             std=(0.5, 0.5, 0.5))])
+
+    def __len__(self):
+        return len(self.labels)
+
+
+    def __getitem__(self, idx):
+        anchor_label = self.labels[idx]
+
+        anchor_ids = np.where(self.labels == anchor_label)[0]
+
+        anchor_idx = np.random.choice(anchor_ids)
+        positive_idx = np.random.choice(anchor_ids)
+
+        anchor_img = self.images[anchor_idx]
+        positive_img = self.images[positive_idx]
+
+        anchor_img = self.transforms(anchor_img)
+        positive_img = self.transforms(positive_img)
+
+        anchor_label = self.labels[anchor_idx].astype(np.int_)
+        positive_label = self.labels[positive_idx].astype(np.int_)
+
+        return anchor_img, positive_img, anchor_label, positive_label
 
 
 if __name__ == "__main__":
